ef5019bec799794e64d97c814b2cd444b48ee6ef
Yara Erazo <yaraerazo@gmail.com>
2016-02-13 17:04:29 -0600

    no more code errors, but not all results correct

--- exercise-1/exercise-1.js
+++ exercise-1/exercise-1.js
@@ -4,16 +4,16 @@ var realObj1 = JSON.parse(jsonString1);
 console.log(realObj1.results); // Should log [1, 2, 3, 4, 5]
 
 //1.2
-var jsonString2 = '1, 2, 3, 4, 5, 6';
+var jsonString2 = '{ "results" : [1, 2, 3, 4, 5, 6] }';
 var realObj2 = JSON.parse(jsonString2);
 console.log(Array.isArray(realObj2)); // Should log 'true'
 
 //1.3
-var jsonString3 = "{ 'results' : [1, 2, 3, 4, 5, 6] }";
+var jsonString3 = '{ "results" : [1, 2, 3, 4, 5, 6] }';
 var realObj3 = JSON.parse(jsonString3);
-console.log(realObj3); // Should log [1, 2, 3, 4, 5]
+console.log(realObj3.results); // Should log [1, 2, 3, 4, 5]
 
 //1.4
-var jsonString4 = "{ status : 'true' }";
-var realObj4 = JSON.parse(jsonString3);
+var jsonString4 = '{ "status" : "true" }';
+var realObj4 = JSON.parse(jsonString4);
 console.log(typeof realObj4.status); // Should log 'boolean'
