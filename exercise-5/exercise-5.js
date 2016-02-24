6695f5ca322769b780e309304ba96d267c5962e6
Yara Erazo <yaraerazo@gmail.com>
2016-02-23 18:52:31 -0600

    preparing to publish

--- exercise-5/exercise-5.js
+++ exercise-5/exercise-5.js
@@ -9,6 +9,7 @@ $(document).ready(function(){
 		    }, function(responseData){
 		    	var lat = responseData.results[0].geometry.location.lat;
 		    	var lng = responseData.results[0].geometry.location.lng;
+		    	console.log(responseData);
 		    	console.log(lat, lng);
 			    $.get('https://api.forecast.io/forecast/975752a53943e64486a7af61e9c38ada/' + lat + ',' + lng, {
 			    }, function(responseData){
